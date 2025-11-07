0b97842686fa938217080a16f691242beb691296
linzuxuan <linzuxuan1@hisilicon.com>
2025-11-07 16:17:24 +0800

    Add MRSA_FLAG control

--- src/block_param.py
+++ src/block_param.py
@@ -59,7 +59,7 @@ def NRSA(N, NR_MAIN):
 
     return NR_MAIN_LOOPS, NR_REMAIN, NR_REMAIN_LOOPS
 
-def MRSA(M, NR):
+def MRSA(M, NR, MRSA_FLAG):
     MR_MAIN = min(6, (SIMD_REG_NUM - max(4, NR)) // (NR + 1))
     logger.debug(f"MR_MAIN = min(6, (32 - max(4, NR)) // (NR + 1)) = min(6, (32 - max(4, {NR})) // ({NR} + 1)) = {MR_MAIN}")
     logger.debug(f"MR_MAIN = {MR_MAIN} (最小值是6（为什么？）， 32是SIMD寄存器总量，NR不足SIMD_LANE的，按照SIMD_LANE计算)")
@@ -71,49 +71,50 @@ def MRSA(M, NR):
     logger.debug(f"MR_REMAIN_LOOPS = 1 if MR_REMAIN else 0")
     logger.debug(f"MR_REMAIN_LOOPS = 1 if {MR_REMAIN} else 0")
     logger.debug(f"MR_REMAIN_LOOPS = {MR_REMAIN_LOOPS} (M方向的剩余循环)")
-    # if MR_MAIN == 5 :
-    #   if MR_REMAIN == 1 :
-    #     if MR_MAIN_LOOPS >= 3 :
-    #       MR_MAIN_LOOPS -= 3
-    #       MR_REMAIN = 4
-    #       MR_REMAIN_LOOPS = 4
-    #     elif MR_MAIN_LOOPS >= 1 :
-    #       MR_MAIN_LOOPS -= 1
-    #       MR_REMAIN = 3
-    #       MR_REMAIN_LOOPS = 2
-    #   elif MR_REMAIN == 2 and MR_MAIN_LOOPS >= 2 :
-    #     MR_MAIN_LOOPS -= 2
-    #     MR_REMAIN = 4
-    #     MR_REMAIN_LOOPS = 3
-    #   elif MR_REMAIN == 3 and MR_MAIN_LOOPS >= 1 :
-    #     MR_MAIN_LOOPS -= 1
-    #     MR_REMAIN = 4
-    #     MR_REMAIN_LOOPS = 2
-    # elif MR_MAIN == 4 :
-    #   if MR_REMAIN == 1 and MR_MAIN_LOOPS >= 2 :
-    #     MR_MAIN_LOOPS -= 2
-    #     MR_REMAIN = 3
-    #     MR_REMAIN_LOOPS = 3
-    #   elif MR_REMAIN == 2 and MR_MAIN_LOOPS >= 1 :
-    #       MR_MAIN_LOOPS -= 1
-    #       MR_REMAIN = 3
-    #       MR_REMAIN_LOOPS = 2
-    # elif MR_MAIN == 3 and MR_REMAIN == 1 and MR_MAIN_LOOPS >= 1 :
-    #   MR_MAIN_LOOPS -= 1
-    #   MR_REMAIN = 2
-    #   MR_REMAIN_LOOPS = 2
+    if MRSA_FLAG == 1:
+        if MR_MAIN == 5 :
+          if MR_REMAIN == 1 :
+            if MR_MAIN_LOOPS >= 3 :
+              MR_MAIN_LOOPS -= 3
+              MR_REMAIN = 4
+              MR_REMAIN_LOOPS = 4
+            elif MR_MAIN_LOOPS >= 1 :
+              MR_MAIN_LOOPS -= 1
+              MR_REMAIN = 3
+              MR_REMAIN_LOOPS = 2
+          elif MR_REMAIN == 2 and MR_MAIN_LOOPS >= 2 :
+            MR_MAIN_LOOPS -= 2
+            MR_REMAIN = 4
+            MR_REMAIN_LOOPS = 3
+          elif MR_REMAIN == 3 and MR_MAIN_LOOPS >= 1 :
+            MR_MAIN_LOOPS -= 1
+            MR_REMAIN = 4
+            MR_REMAIN_LOOPS = 2
+        elif MR_MAIN == 4 :
+          if MR_REMAIN == 1 and MR_MAIN_LOOPS >= 2 :
+            MR_MAIN_LOOPS -= 2
+            MR_REMAIN = 3
+            MR_REMAIN_LOOPS = 3
+          elif MR_REMAIN == 2 and MR_MAIN_LOOPS >= 1 :
+              MR_MAIN_LOOPS -= 1
+              MR_REMAIN = 3
+              MR_REMAIN_LOOPS = 2
+        elif MR_MAIN == 3 and MR_REMAIN == 1 and MR_MAIN_LOOPS >= 1 :
+          MR_MAIN_LOOPS -= 1
+          MR_REMAIN = 2
+          MR_REMAIN_LOOPS = 2
     
     logger.debug(f"Adjusted MR_MAIN_LOOPS = {MR_MAIN_LOOPS}")
     logger.debug(f"Adjusted MR_REMAIN = {MR_REMAIN}")
     logger.debug(f"Adjusted MR_REMAIN_LOOPS = {MR_REMAIN_LOOPS}")
     return MR_MAIN, MR_MAIN_LOOPS, MR_REMAIN, MR_REMAIN_LOOPS
 
-def RBSA(M, N, NR_MAIN):
+def RBSA(M, N, NR_MAIN, MRSA_FLAG):
     logger.debug(f"开始计算N方向的主要参数...")
     NR_MAIN_LOOPS, NR_REMAIN, NR_REMAIN_LOOPS = NRSA(N, NR_MAIN)
     logger.debug(f"开始计算N的主循环中M方向的主要参数...")
-    NR_MAIN_MR_MAIN, NR_MAIN_MR_MAIN_LOOPS, NR_MAIN_MR_REMAIN, NR_MAIN_MR_REMAIN_LOOPS = MRSA(M, NR_MAIN) if NR_MAIN_LOOPS else (0,0,0,0)
+    NR_MAIN_MR_MAIN, NR_MAIN_MR_MAIN_LOOPS, NR_MAIN_MR_REMAIN, NR_MAIN_MR_REMAIN_LOOPS = MRSA(M, NR_MAIN, MRSA_FLAG) if NR_MAIN_LOOPS else (0,0,0,0)
     logger.debug(f"开始计算N的剩余循环中M方向的主要参数...")
-    NR_REMAIN_MR_MAIN, NR_REMAIN_MR_MAIN_LOOPS, NR_REMAIN_MR_REMAIN, NR_REMAIN_MR_REMAIN_LOOPS = MRSA(M, NR_REMAIN) if NR_REMAIN_LOOPS else (0,0,0,0)
+    NR_REMAIN_MR_MAIN, NR_REMAIN_MR_MAIN_LOOPS, NR_REMAIN_MR_REMAIN, NR_REMAIN_MR_REMAIN_LOOPS = MRSA(M, NR_REMAIN, MRSA_FLAG) if NR_REMAIN_LOOPS else (0,0,0,0)
 
     return NR_MAIN_LOOPS, NR_REMAIN, NR_REMAIN_LOOPS, NR_MAIN_MR_MAIN, NR_MAIN_MR_MAIN_LOOPS, NR_MAIN_MR_REMAIN, NR_MAIN_MR_REMAIN_LOOPS, NR_REMAIN_MR_MAIN, NR_REMAIN_MR_MAIN_LOOPS, NR_REMAIN_MR_REMAIN, NR_REMAIN_MR_REMAIN_LOOPS
