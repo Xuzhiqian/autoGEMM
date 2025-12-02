b4f0b20d75203c442bb1e2bd09017fb483588611
linzuxuan <linzuxuan1@hisilicon.com>
2025-12-02 21:39:30 +0800

    Adding a map for size to func

--- src/tvm_tuner/build_kernel_params_list.py
+++ src/tvm_tuner/build_kernel_params_list.py
@@ -22,23 +22,83 @@ if __name__ == "__main__":
     cc_code = f"""#ifndef __KERNEL_PARAMS_LIST_H_
 #define __KERNEL_PARAMS_LIST_H_
 
-#include <list>
+#include <iostream>
+#include <map>
+#include <cmath>
+
+#include "tvm/runtime/module.h"
+#include "tvm/runtime/packed_func.h"
 
 namespace KernelParams
 {{
-    struct SimpleStruct {{
+    struct Key {{
+        int M;
+        int N;
+        int K;
+
+        Key() : M(0), N(0), K(0) {{}}
+        Key(int m, int n, int k) : M(m), N(n), K(k) {{}}
+
+        // 重载 < 运算符，用于 map 的比较
+        bool operator<(const Key& other) const {{
+            if (M != other.M) {{
+                return M < other.M;
+            }} else if (N != other.N) {{
+                return N < other.N;
+            }} else {{
+                return K < other.K;
+            }}
+        }}
+    }};
+
+    struct Value {{
         int M;
         int N;
         int K;
         int nc;
         int kc;
         int padding_size;
-        SimpleStruct(int M, int N, int K, int nc, int kc, int padding_size)
-            : M(M), N(N), K(K), nc(nc), kc(kc), padding_size(padding_size){{}}
+        int nc_ceil;
+        int packedB_size;
+        int64_t A_shape[2];
+        int64_t B_shape[2];
+        int64_t packedB_shape[4];
+        int64_t C_shape[2];
+
+        tvm::runtime::PackedFunc pack_func;
+        tvm::runtime::PackedFunc func;
+
+        Value() : M(0), N(0), K(0), nc(0), kc(0), padding_size(0) {{}}
+        Value(int m, int n, int k, int nc, int kc, int p) : M(m), N(n), K(k), nc(nc), kc(kc), padding_size(p) {{
+            nc_ceil = ((nc - 1) / padding_size + 1) * padding_size;
+            packedB_size = K * (N / nc) * nc_ceil;
+            A_shape[0] = M;
+            A_shape[1] = K;
+            B_shape[0] = K;
+            B_shape[1] = N;
+            packedB_shape[0] = K / kc;
+            packedB_shape[1] = N / nc;
+            packedB_shape[2] = kc;
+            packedB_shape[3] = nc_ceil;
+            C_shape[0] = M;
+            C_shape[1] = N;
+
+            std::string base_name = "GEMM_" + std::to_string(M) + "X" + std::to_string(N) + "X" + std::to_string(K);
+            std::string mod_name = base_name + "_kernel.so";
+            std::string func_name = "OP_" + base_name;
+            std::string pack_func_name = func_name + "_packB";
+            std::string loading_path = "/home/linzuxuan/autoGEMM/autoGEMM/data/tune_output/build/library/" + mod_name;
+
+            tvm::runtime::Module mod_tvmlib = tvm::runtime::Module::LoadFromFile(loading_path);
+            pack_func = mod_tvmlib.GetFunction(pack_func_name);
+            func = mod_tvmlib.GetFunction(func_name);
+        }}
     }};
-    static std::list<SimpleStruct> params_list;
-    static void CreateList()
-    {{
+
+    static std::map<Key, Value> mapping;
+
+    static void CreateMap() {{
+        if (mapping.empty()) {{
 """
     with open(input_file_path, 'r') as load_f:
         for line in load_f:
@@ -56,8 +116,9 @@ namespace KernelParams
                     kc = param_value[-1]
                 if param_name == "padding_size":
                     padding_size = param_value
-            cc_code+=f"""        params_list.push_back(SimpleStruct({M}, {N}, {K}, {nc}, {kc}, {padding_size}));\n"""
-    cc_code += f"""    }}
+            cc_code+=f"""            mapping[{{{M}, {N}, {K}}}] = {{{M}, {N}, {K}, {nc}, {kc}, {padding_size}}};\n"""
+    cc_code += f"""        }}
+    }}
 }};
 #endif"""
 
