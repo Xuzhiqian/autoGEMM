b93d433b4400e5168ef282d401c5dbeaa413d17c
linzuxuan <linzuxuan1@hisilicon.com>
2025-12-12 14:34:05 +0800

    Make the test of pipeline become one line

--- src/micro_kernel/make_c_file_asm_pipeline_experiment.py
+++ src/micro_kernel/make_c_file_asm_pipeline_experiment.py
@@ -19,20 +19,26 @@ from makefile_generator import makefile_generator
 M = int(sys.argv[1]) # M，N，K来自命令行参数
 N = int(sys.argv[2])
 K = int(sys.argv[3])
-UNROLL_K = int(sys.argv[4]) # K方向的循环展开次数
-NR_MAIN = int(sys.argv[5]) # N方向的主循环大小
-REPEAT = int(sys.argv[6]) # 测试中循环的次数
-PIPELINE_STRATEGY_LEVEL = int(sys.argv[7]) # 流水优化策略等级
-MRSA_FLAG = int(sys.argv[8]) # 额外的分块值微调
+lda = int(sys.argv[4])
+ldb = int(sys.argv[5])
+ldc = int(sys.argv[6])
+UNROLL_K = int(sys.argv[7]) # K方向的循环展开次数
+NR_MAIN = int(sys.argv[8]) # N方向的主循环大小
+REPEAT = int(sys.argv[9]) # 测试中循环的次数
+PIPELINE_STRATEGY_LEVEL = int(sys.argv[10]) # 流水优化策略等级
+MRSA_FLAG = int(sys.argv[11]) # 额外的分块值微调
 
 UNIQ_ID_LEN = 8
 uniq_id = "".join(random.choices(string.ascii_uppercase, k=UNIQ_ID_LEN))
 logger.debug(f"unique_id: {uniq_id} (用于作为接口名唯一性标识)")
-f = open('c_file_asm.cpp','w')
-logger.debug(f"调用gemm_MxNxK_impl传入M={M}, N={N}, K={K}，A，B，C都是行主序")
-f.write(gemm_MxNxK_impl(M, N, K, K, N, N, uniq_id, repeat=REPEAT, pipeline_strategy_level=PIPELINE_STRATEGY_LEVEL, UNROLL_K=UNROLL_K, NR_MAIN=NR_MAIN, MRSA_FLAG = MRSA_FLAG))
+current_directory = os.path.dirname(os.path.abspath(__file__))
+c_file_asm_cpp_path = os.path.join(current_directory, f'../../test/micro_kernel_tester/c_file_asm.cpp')
+f = open(c_file_asm_cpp_path,'w')
+logger.debug(f"Calling gemm_MxNxK_impl(M={M}, N={N}, K={K}，lda={lda}，ldb={ldb}，ldc={ldc})")
+f.write(gemm_MxNxK_impl(M, N, K, lda, ldb, ldc, uniq_id, repeat=REPEAT, pipeline_strategy_level=PIPELINE_STRATEGY_LEVEL, UNROLL_K=UNROLL_K, NR_MAIN=NR_MAIN, MRSA_FLAG=MRSA_FLAG))
 f.close()
 
-f = open('Makefile', 'w')
+makefile_path = os.path.join(current_directory, f'../../test/micro_kernel_tester/Makefile')
+f = open(makefile_path, 'w')
 f.write(makefile_generator())
 f.close()
