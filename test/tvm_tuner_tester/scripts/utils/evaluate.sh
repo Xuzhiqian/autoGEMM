f5e5cb59e05172fd2d6b0f0f38e4a03cfbe3f4de
linzuxuan <linzuxuan1@hisilicon.com>
2026-01-28 11:40:25 +0800

    Make evaluate scripts simpler (Just set day and time)

--- test/tvm_tuner_tester/scripts/utils/evaluate.sh
+++ test/tvm_tuner_tester/scripts/utils/evaluate.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 set -e
 
+# parameter setting
+arch=a64fx
+threads=1
+repeats=1
+module=jd_mmoe
+day=0
+time=0
+
+for arg in "$@"; do
+    shift
+    case "$arg" in
+        --arch) arch="$1" ;;
+        --threads) threads="$1" ;;
+        --repeats) repeats="$1" ;;
+        --module) module="$1" ;;
+        --day) day="$1" ;;
+        --time) time="$1" ;;
+    esac
+done
+
+echo "-- arch is ${arch}"
+echo "-- threads is ${threads}"
+echo "-- repeats is ${repeats}"
+echo "-- module is ${module}"
+echo "-- day is ${day}"
+echo "-- time is ${time}"
+
 # directory setting
 tmp=`dirname $0`
 PROJECT_ROOT=`cd $tmp/../../../..; pwd`
 cd ${PROJECT_ROOT}
 echo "Project root: $PROJECT_ROOT"
-app="dlrm"
-# day="20251121"
-# time="20251120165501" # neon
-# time="20251121161600" # sve
-# day="20251126"
-# time="20251126165559" # 第一次ncopy调优：100次
-# time="20251126171752" # 第二次ncopy调优： 1000次
-# day="20251201"
-# time="20251201152719" # 比较接近kdnn性能的版本
-day="20251202"
-# time="20251202113119" # 调MNK次序的版本, segmentation fault
-time="20251202214055" # 优化2000轮的版本，结果并没有current_best好
-tune_output_path=$PROJECT_ROOT/data/scheduler_house/$app/$day/$time
-# tune_output_path=$PROJECT_ROOT/data/scheduler_house/$app/current_best
+WORKING_DIR=$PROJECT_ROOT/data/tune_output
 src_path=$PROJECT_ROOT/src
-build_output_path=$tune_output_path/build
-
-# parameter setting
-arch=$1
-threads=$2
-repeats=$3
+schedule_setting_path=$PROJECT_ROOT/data/scheduler_house/$module/$day/$time
+if [[ "$day" == "0" ]] && [[ "$time" == "0" ]]; then
+    schedule_setting_path=$PROJECT_ROOT/data/scheduler_house/$module/current_best
+fi
 
 if [ "${threads}" == "1" ]; then
     parallel=""
@@ -37,45 +49,49 @@ else
     exit -1
 fi
 
-export OMP_NUM_THREADS=${threads}
-
-if [[ -d $build_output_path ]]; then
-    rm -rf $build_output_path
+# creating directories
+if [[ -d $WORKING_DIR ]]; then
+    rm -rf $WORKING_DIR
 fi
-mkdir -p $build_output_path
-mkdir -p $build_output_path/gemm_obj
-mkdir -p $build_output_path/generated_micro_kernel
-mkdir -p $build_output_path/library
+mkdir -p $WORKING_DIR
+mkdir -p $WORKING_DIR/build
+mkdir -p $WORKING_DIR/build/gemm_obj                # 存放调优过程产生的二进制
+mkdir -p $WORKING_DIR/build/gemm_source             # 存放调优过程产生的c文件
+mkdir -p $WORKING_DIR/build/generated_micro_kernel  # 存放调优过程产生的micro_kernel内嵌汇编文件
+mkdir -p $WORKING_DIR/build/library                 # 存放调优过程产生的tvm序列化library
 
-MNK_file=${tune_output_path}/MNK.txt 
-scheduler_log=${tune_output_path}/scheduler_summary.log
-scheduler_log_output=$build_output_path/scheduler_summary.log
+MNK_file=${schedule_setting_path}/MNK.txt                       # 存放调优过程涉及的MNK规模
+scheduler_log=${schedule_setting_path}/scheduler_summary.log    # 存放最终调优结果
 
+# starting evaluation
+
+## set environment
+export OMP_NUM_THREADS=${threads}
 export PYTHONPATH=$PYTHONPATH:$src_path:$src_path/tvm_tuner:$src_path/micro_kernel
 export TVM_CC=clang++
 
-summarize_scheduler_path=$PROJECT_ROOT/src/tvm_tuner/summarize_scheduler.py
+## creating kernel_params_list.hpp for wrapper and tests
 build_kernel_params_list_path=$PROJECT_ROOT/src/tvm_tuner/build_kernel_params_list.py
-python $summarize_scheduler_path --input $scheduler_log --output $scheduler_log_output
-python $build_kernel_params_list_path --scheduler_log $scheduler_log_output
+python $build_kernel_params_list_path --scheduler_log $scheduler_log
 
+## creating benchmark tests for autotvm generated kernel
 tester_src_path=$PROJECT_ROOT/test/tvm_tuner_tester/src
 cd $tester_src_path
 make -s
 
+## evaluation
 evaluate_scheduler_path=$PROJECT_ROOT/src/tvm_tuner/evaluate_scheduler.py
-benchmark_kernel_path=$tester_src_path/benchmark_kernel
-echo "benchmark_kernel_path: $benchmark_kernel_path"
-cnt=0
 cat $MNK_file | while read line
 do
     M=`echo $line | awk '{print $1}'`
     N=`echo $line | awk '{print $2}'`
     K=`echo $line | awk '{print $3}'`
-    python $evaluate_scheduler_path -m ${M} -n ${N} -k ${K} -a ${arch} ${parallel} --scheduler_log ${scheduler_log_output}
-    let cnt+=1
+    python $evaluate_scheduler_path -m ${M} -n ${N} -k ${K} -a ${arch} ${parallel} --best_record_file ${scheduler_log}
 done
 
+## benchmarking
+benchmark_kernel_path=$tester_src_path/benchmark_kernel
+echo "benchmark_kernel_path: $benchmark_kernel_path"
 cat $MNK_file | while read line
 do
     M=`echo $line | awk '{print $1}'`
@@ -86,4 +102,4 @@ do
     $benchmark_kernel_path ${M} ${N} ${K} ${repeats}
 done
 
-touch $build_output_path/build.over
+touch $WORKING_DIR/build/build.over
